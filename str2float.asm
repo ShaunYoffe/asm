8d68fe4072df6d4f607c5b350c8fd44e84e4481b
Shaun <shon@duck.com>
2024-09-29 12:39:00 +0300

    finish str2float

--- str2float.asm
+++ str2float.asm
@@ -7,12 +7,15 @@ DATASEG
 	count DB (?)
 	buffer DB 255 dup (?)
 	tens DW 1, 10, 100, 1000, 10000
+	FPUIN DD 0
+	FPUOUT DD 0
+	asdf DD 50.3
 CODESEG
 
 	; input: di = offset of leftmost digit in string
 	; input: cx = number of digits to convert (0-5)
-	; destroys ax, bx, cx, si, di
-	; output: dx = value
+	; destroys ax, bx, cx, si, di, flags
+	; output: ADDS value to dx (dx = dx + value)
 	proc str2int
 		add di, cx
 		dec di
@@ -37,6 +40,7 @@ CODESEG
 	endp
 
 	; input: bx = offset of string start
+	; destroys: flags
 	; output: si = offset of dot INSIDE string offset
 	proc finddot
 		xor si, si
@@ -48,23 +52,58 @@ CODESEG
 		found:
 		ret
 	endp
+
+	; input: bx = string offset
+	; destroys: ax, bx, cx, dx, di, si
+	; uses 2 FPU registers
+	; output: st(0) = value
+	proc str2float
+		call finddot          ; offset buffer + si = dot address
+		mov di, offset buffer
+		mov cx, si
+		push cx
+		call str2int
+		mov ax, dx
+	
+		pop cx                 ; preserve dot offset
+		mov di, offset buffer 
+		add di, cx             
+		inc di                 ; di = dot address + 1
+		neg cx
+		add cl, [byte offset count]
+		adc ch, 0
+		dec cl                 ; cl = number of digits to convert
+		xor dx, dx
+		push cx
+		push ax
+		call str2int
+		pop ax
+		pop di
+		add di, di
+		mov [word offset FPUIN], dx
+		fild [word offset FPUIN]
+		mov di, [tens + di]
+		mov [word offset FPUIN], di
+		fild [word offset FPUIN]
+		fdivp st(1),st
+		mov [word offset FPUIN], ax
+		fild [dword offset FPUIN]
+		fxch st(1)
+		faddp st(1), st
+		ret
+	endp
 start:
 	mov ax, @data
 	mov ds, ax
+
 
 	mov dx, offset max
 	mov ah, 0Ah
 	int 21h
 
 	mov bx, offset buffer
-	call finddot          ; buffer + si = dot offset
-	mov di, offset buffer
-	mov cx, si
-	push cx
-
-	call str2int
-	pop cx
-	mov di, offset buffer 
+	fld [asdf]
+	call str2float
 exit:
 	mov ax, 4c00h     
 	int 21h
